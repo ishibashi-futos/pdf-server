c599e03c4c27b9a962cacaeee0d583693e0e7b99
ishibashi.futos <ishibashi.futos@jp.fujitsu.com>
2019-06-06 14:31:47 +0900

    PDFデータを取得するControllerクラスを追加。Dao層がまだなので頑張る

---
+++ src/Controllers/AssetsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using pdf_server.Services;
+using pdf_server.Dao;
+
+namespace pdf_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssetsController : ControllerBase
+    {
+        private PDFService service;
+        public AssetsController()
+        {
+            this.service = new PDFService(new TestRepository());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}/pdf.pdf")]
+        public ActionResult Get(string id, [FromQuery]int serial)
+        {
+            var bytes = this.service.GetPDFData(id, serial);
+            return this.File(bytes, "application/octet-stream");
+        }
+    }
+
+}
---
+++ src/Services/PDFService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using pdf_server.Dao;
+
+namespace pdf_server.Services
+{
+    public class PDFService
+    {
+        private IRepository repository;
+        public PDFService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public byte[] GetPDFData(string id, int serial)
+        {
+            // TODO: modelに合致するSerialのPathを取得するように変更する
+            var filePath = this.repository.findById(id);
+            using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                var bytes = new byte[fs.Length];
+                fs.Read(bytes, 0, (int)fs.Length);
+                return bytes;
+            }
+        }
+    }
+
+}
