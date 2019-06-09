5bb4df02fccab5af03bb944d36946f12cd8fc16d
ishibshi.futos <ishibshi.futos@outlook.com>
2019-06-09 15:57:45 +0900

    修正？

---
+++ src/Services/StorageStatusService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using pdf_server.Dao;
+
+namespace pdf_server.Services
+{
+    public class StorageStatusService
+    {
+        private IStorageStatusDao repository;
+        public StorageStatusService(IStorageStatusDao repository)
+        {
+            this.repository = repository;
+        }
+
+        public List<StorageStatus> GetData(string customerCode, string customerName, string customerNameKana)
+        {
+            if (!string.IsNullOrEmpty(customerCode))
+                return this.repository.findByCustomerCode(customerCode);
+            if (!string.IsNullOrEmpty(customerName))
+                return this.repository.findByCustomerNameLike(customerName);
+            if (!string.IsNullOrEmpty(customerNameKana))
+                return this.repository.findByCustomerNameLike(customerNameKana);
+            return this.repository.findAll();
+        }
+
+    }
+
+}
